9d6410453fa15ad411a2251678622c36bbe3043a
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-03-01 21:54:21 -0600

    Creación

---
+++ LABORATORIO/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LABORATORIO.Entidades
+{
+    public class Persona
+    {
+        public String? Nombre { get; set; }
+        public int Edad { get; set; }
+        public String? Sexo { get; set; }  
+        public double Peso { get; set; }
+        public double Altura { get; set; } 
+    }
+}
